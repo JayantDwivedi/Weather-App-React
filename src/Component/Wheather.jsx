90e18270138783ff7369b71fdbe43d9f064d3db0
Jayant Dwivedi <jayantdwivedi19@gmail.com>
2021-06-29 15:23:56 +0530

    Weather App

---
+++ src/Component/Wheather.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import {useState} from 'react';
+import {Form,FormControl,Button} from 'react-bootstrap';
+import './style.css';
+
+
+export default function Wheather() {
+
+    const [data,setData] = useState();
+    const [city, setCity] = useState("");
+
+
+    const fetchAPI= async()=>{
+        if(city === "") {
+            alert("Enter City Name");
+        }
+        else{
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=9981f256b83dc3a6f9e0c74264037ce9`;
+            const reccivedata = await fetch(url);
+            const datajson = await reccivedata.json();
+            // console.log(data.message);
+            setData(datajson);
+            // return datajson;
+        }
+        
+    }
+
+    // const validate=() => {
+    //     // city = city.trim();
+    //     if(city === "") {
+    //         alert("Enter City Name");
+    //     }
+    //     else {
+    //         const res = fetchAPI
+    //         if (res.cod !== 200) {
+    //             alert("City Not found")
+              
+    //         }
+    //         else {
+    //             setData(res)
+    //         }
+    //     }
+    // }
+    
+    
+    // const {}
+
+    return (
+        <div className="container my-4 px-0">
+
+        
+            <div className="row">
+                <div className="col-lg-3  opacity-outer image">
+                        
+                </div>
+                                   
+                <div className="col-lg-9 content">
+                     <div className="mx-0">
+                        <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Enter City Name"
+                            className="mx-2 my-2"
+                            aria-label="Search"
+                            name="city"
+                            onChange={(e)=>setCity(e.target.value)}
+                        />
+                        <Button variant="success" className="my-2"  onClick={fetchAPI}>Search</Button>                        
+                        </Form>
+                    </div> 
+                    <div className="mx-4 text-center content-data">
+                    {
+                            !(data)?(
+                                <div class="alert alert-danger" role="alert">
+                                        City Not Found
+                                        {/* {data.message} */}
+                                </div>
+                            ):(
+                                 <ul>
+                                    <li>  <i class="fas fa-map-marker-alt"></i>   {data.name}</li>
+                                    <li>  Temp :  {data.main.temp} </li>
+                                    <li> <i class="fas fa-temperature-half"></i> Feel Like:  {data.main.feels_like}°C</li>
+                                    <li> <i class="fas fa-temperature-low"></i> Min Temp:  {data.main.temp_min}°C</li>
+                                    <li> <i class="fas fa-temperature-high"></i> Max Temp:  {data.main.temp_max}°C</li>
+                                    <li> <i class="fas fa-tint"></i> Humidity: {data.main.humidity}%</li>
+                                    <li> <i class="fas fa-cloud"></i> Sky: {data.weather[0].description}</li>
+                                    {/* <li>{data.cod}</li> */}
+                                </ul>
+                                
+                            )
+                        }
+                        
+                    </div>
+
+                    </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/context/context.jsx
@@ -0,0 +1,47 @@
+data = 
+{
+"coord": {
+"lon": 79.0167,
+"lat": 27.2333
+},
+"weather": [
+{
+"id": 800,
+"main": "Clear",
+"description": "clear sky",
+"icon": "01n"
+}
+],
+"base": "stations",
+"main": {
+"temp": 310.08,
+"feels_like": 310.51,
+"temp_min": 310.08,
+"temp_max": 310.08,
+"pressure": 1000,
+"humidity": 29,
+"sea_level": 1000,
+"grnd_level": 983
+},
+"visibility": 10000,
+"wind": {
+"speed": 4.2,
+"deg": 81,
+"gust": 7.24
+},
+"clouds": {
+"all": 0
+},
+"dt": 1624463507,
+"sys": {
+"country": "IN",
+"sunrise": 1624405820,
+"sunset": 1624455708
+},
+"timezone": 19800,
+"id": 1264292,
+"name": "Mainpuri",
+"cod": 200
+}
+
+export default data;
